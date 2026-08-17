797c9ecbf3d02eb51aa0dc822570170048f0cab4
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2026-08-17 16:38:48 +0300

    add jsconfig.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 import { Route, Routes } from "react-router";
-import { Layout } from "./componets/Layout";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
