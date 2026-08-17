ab24e07b5aa3ab3de7cb5838a6a467e51e0709d9
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2026-08-17 11:37:16 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+//! IF you are using routes like react router add base: /Repo_name/
+//! https://vite.dev/guide/static-deploy#github-pages
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "",
+});
